af91208ecb275218e019eba7330725c24189ca7d
hansjiang <oneal32xx@hotmail.com>
2023-07-18 22:52:30 +0800

    update Algolia config

--- HansRenolution/docusaurus.config.js
+++ HansRenolution/docusaurus.config.js
@@ -146,5 +146,34 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         darkTheme: darkCodeTheme,
         additionalLanguages: ['csharp'],
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'RK13VLDJAB',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '8521ff5e8d4e176eed5005c0bf179584',
+  
+        indexName: 'hansrevolution',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
     }),
 });
